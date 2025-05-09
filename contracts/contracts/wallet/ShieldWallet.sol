4a60ff30f4415a3e7b44edd895cc1b3d5283deed
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-05-10 00:46:01 +0200

    :sparkles: feat: add Shield Wallet

---
+++ contracts/contracts/wallet/ShieldWallet.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnersManager} from "./OwnersManager.sol";
+import {ProposerManager} from "./ProporserManager.sol";
+import {CheckedExecutor} from "./CheckedExecutor.sol";
+import {Timelock} from "./Timelock.sol";
+import {FallbackManager} from "./FallbackManager.sol";
+
+contract ShieldWallet is
+    Initializable,
+    UUPSUpgradeable,
+    OwnersManager,
+    ProposerManager,
+    CheckedExecutor,
+    Timelock,
+    FallbackManager
+{
+    constructor() {
+        _disableInitializers();
+        // TODO: Set All thresholds to 1 to avoid transaction execution in the implementation
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               UUPS PROXY
+    //////////////////////////////////////////////////////////////*/
+
+    // TODO: Add initialization data
+    function initialize(
+        address[] calldata _owners,
+        uint256 _managementThreshold,
+        uint256 _executionThreshold,
+        uint256 _revocationThreshold,
+        address _fallbackHandler,
+        address _proposer,
+        uint256 _delay
+    ) public initializer {
+        __UUPSUpgradeable_init();
+
+        _setupOwnersAndThresholds(
+            _owners,
+            _managementThreshold,
+            _executionThreshold,
+            _revocationThreshold
+        );
+
+        if (_fallbackHandler != address(0))
+            _setFallbackHandler(_fallbackHandler);
+
+        if (_proposer != address(0)) _setProposer(_proposer);
+    }
+
+    // TODO: Check if we need this here upgradeToAndCall is already onlyProxy
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlySelf {}
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event ExecutionProposed(
+        bytes32 indexed id,
+        bytes32 mode,
+        bytes executionData,
+        ThresholdType indexed threshold,
+        uint256 timestamp
+    );
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error UnauthorizedProposer();
+
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+    modifier onlyProposerOrOwner() {
+        require(
+            owners[msg.sender] != address(0) || msg.sender == getProposer(),
+            UnauthorizedProposer()
+        );
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            EXECUTION LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    // Struct that is EIP-712 signed by the owners EOA
+    struct ExecutionId {
+        bytes32 executionId;
+    }
+
+    /// @dev Adds an execution to the queue.
+    /// See: https://eips.ethereum.org/EIPS/eip-7579
+    /// ShieldWallet EIP-7579 `executionData` encodings:
+    /// - `executionData` encoding (single call): abi.encodePacked of `Call` fields.
+    /// - `executionData` encoding (batch): abi.encoded array of `Call` structs
+    function propExecution(
+        bytes32 mode,
+        bytes calldata executionData
+    ) external onlyProposerOrOwner {
+        (bytes32 executionId, ThresholdType threshold) = _validateExecution(
+            mode,
+            executionData
+        );
+
+        _schedule(executionId);
+
+        emit ExecutionProposed(
+            executionId,
+            mode,
+            executionData,
+            threshold,
+            block.timestamp
+        );
+    }
+
+    /// @dev Removes an execution from the queue.
+    function revoExecution() external {
+        // TODO: Adjust arguments
+        //TODO: Check revoker threshold
+    }
+
+    /// @dev Executes an execution from the queue.
+    function execExecution(bytes calldata signatures) external {
+        // TODO: Adjust arguments
+        // TODO: Check executor threshold
+    }
+}
