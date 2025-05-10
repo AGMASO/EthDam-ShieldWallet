9b2d5eaad53facd028fb4c351358713c292ae111
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-05-10 07:38:29 +0200

    :sparkles: feat: add Shield Wallet Factory

--- contracts/contracts/ShieldWalletFactory.sol
+++ contracts/contracts/ShieldWalletFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import {ERC1967Proxy} from "@openzeppelin/openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 /// @title ShieldWalletFactory
 /// @notice Factory contract for deploying ShieldWallet proxy instances.
